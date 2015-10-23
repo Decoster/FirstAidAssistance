1e1763f62a9d3f5839830f03e67e7292d898314b
Chris Kahmann <chriskahmann@yahoo.com>
2015-10-23 14:36:56 +0200

    changement dans l'appel de la page callHelpActivity

--- app/src/main/java/com/ibm/firstaidassistance/LoginActivity.java
+++ app/src/main/java/com/ibm/firstaidassistance/LoginActivity.java
@@ -3,6 +3,7 @@ package com.ibm.firstaidassistance;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -93,8 +94,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
                 attemptLogin();
+                if(attemptLogin())
+                {
+                    startActivity(new Intent(getApplicationContext(), callHelpActivity.class));
+                }
+                else
+                {
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                }
             }
         });
+
 
         mLoginFormView = findViewById(com.ibm.firstaidassistance.R.id.login_form);
         mProgressView = findViewById(com.ibm.firstaidassistance.R.id.login_progress);
@@ -150,10 +160,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
-    private void attemptLogin() {
+    private boolean attemptLogin() {
 
         if (mAuthTask != null) {
-            return;
+            return false;
         }
 
         // Reset errors.
@@ -189,6 +199,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             cancel = true;
         }
 
+        boolean result = false;
         if (cancel) {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
@@ -199,7 +210,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            result = false;
         }
+        return result;
     }
 
     private boolean isEmailValid(String email) {
