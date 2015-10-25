0123c0fc132620b54a05e619d7d5060a7d7a25a4
Aewolf <ascone.alexandre@hotmail.fr>
2015-10-25 19:00:21 +0100

    debut implementation d'un widget

---
+++ app/src/main/java/com/ibm/firstaidassistance/CallHelpWidget.java
@@ -0,0 +1,17 @@
+package com.ibm.firstaidassistance;
+
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Created by aascone on 25/10/2015.
+ */
+public class CallHelpWidget extends AppWidgetProvider {
+
+
+
+
+}
